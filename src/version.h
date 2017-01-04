25c2d3b3d550c0ce8c4d63941d675c9026bd52a0
Yves Ledermann <yves@LTECHNET.NET>
2017-01-04 22:37:32 +0100

    initial commit

---
+++ src/version.h
@@ -0,0 +1,4 @@
+#define APP_NAME                "itead-sonoff-button"
+#define APP_VERSION             "2.0.1"
+#define APP_AUTHOR              "yves@laedis.ch"
+#define APP_WEBSITE             "http://yves.laedis.ch"
